193415e3661616d575c1b9d0de8e93f7dca898d3
Kevin Huang <yellow.khaos@gmail.com>
2018-07-02 02:29:13 +0000

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,16 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    if name==''
+      result = Student.all
+    else
+      result = Student.all.find_all do |student|
+        student.name.downcase.include?(name.downcase)
+      end
+    end
+    result
+  end
+
 end
