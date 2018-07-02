eb78791cd0c6cee6e9e267ab7ca051c8f0faf698
Kevin Huang <yellow.khaos@gmail.com>
2018-07-02 02:36:43 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,8 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.seach(params[:query])
+    render 'index'
   end
 
   def student_params
